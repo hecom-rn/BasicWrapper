88636444ae8ff825c562518771f9a929144a2b4f
LJJ <lujunjian@hecom.cn>
2020-05-26 16:51:25 +0800

    nav5

--- src/index.tsx
+++ src/index.tsx
@@ -1,43 +1,17 @@
 import React from 'react';
 import { ActivityIndicator, StyleSheet, View } from 'react-native';
 import Foundation from '@hecom/foundation'; // eslint-disable-line
-import Navigation from '@hecom/navigation';
 
 export default (WrappedComponent) => {
     return class extends React.PureComponent {
-        static navigationOptions = WrappedComponent.navigationOptions || {header: null};
+        static navigationOptions = WrappedComponent.navigationOptions || {headerShown: false};
 
         _apiRefresh = (_isApiLoading, _apiLoadingStyle) => {
             this.props.navigation.setParams({_isApiLoading, _apiLoadingStyle});
         };
 
-        constructor(props) {
-            super(props);
-            Navigation.setRefreshNav(props.navigation);
-        }
-
-        componentDidMount() {
-            this.viewDidAppear = this.props.navigation.addListener(
-                'didFocus',
-                () => {
-                    Navigation.setRefreshNav(this.props.navigation);
-                }
-            )
-            this.viewWillDisappear = this.props.navigation.addListener(
-                'willBlur',
-                () => {
-                    Navigation.setRefreshNav(null);
-                }
-            )
-        }
-
-        componentWillUnmount() {
-            this.viewDidAppear.remove();
-            this.viewWillDisappear.remove();
-        }
-
         render() {
-            const params = this.props.navigation.state.params || {};
+            const params = this.props.route.params || {};
             const {_isApiLoading, _apiLoadingStyle} = params;
             delete params._isApiLoading;
             delete params._apiLoadingStyle;
@@ -47,6 +21,7 @@ export default (WrappedComponent) => {
                         {...params}
                         navigation={this.props.navigation}
                         apiRefresh={this._apiRefresh}
+                        route = {this.props.route}
                     />
                     {_isApiLoading && this._renderLoadingView(_apiLoadingStyle)}
                 </View>
