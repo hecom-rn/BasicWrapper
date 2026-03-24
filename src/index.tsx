43875d985e299cfc6cdb4d6d7a601987b2d270a1
tianxuejun <tianxuejun@noreply.gitcode.com>
2026-03-24 16:00:36 +0800

    feat(新建/编辑): image图片也只保留一份
    【移动端】html、iconfont等资源统一为一份： https://devcloud.cn-north-4.huaweicloud.com/projectman/scrum/ea9762777b694cbe8b099d7f6acb453c/task/detail/70074913
    
    工时:3
    缺陷分析:
    问题原因:
    影响范围:

--- src/index.tsx
+++ src/index.tsx
@@ -51,7 +51,7 @@ export default (WrappedComponent, style = undefined) => {
                                 autoPlay={true}
                                 resizeMode={'contain'}
                                 style={{height: 150, width: 150}}
-                                imageAssetsFolder={'lottieLoading'}
+                                imageAssetsFolder={'image/lottieLoading'}
                             />
                 </View>
             );
